63386b8b73754b09c654aa631d2ccaba0afab45c
TranTam <tamvaa1306@gmail.com>
2022-09-18 15:12:13 +0700

    my first class decorator

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
 // Code goes here!
+
+function Logger(constructor: Function) {
+  console.log("Logging...");
+  console.log(constructor);
+}
+
+@Logger
+class Person {
+  name = "Max";
+
+  constructor() {
+    console.log("Creating person object...");
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
